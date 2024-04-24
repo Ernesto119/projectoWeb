bddf096e5e328f3ec0aa1901733f1957a66e991e
Ernesto <ernestojrr30@gmail.com>
2024-04-24 10:03:36 -0400

    add ckeditor

--- posted/models.py
+++ posted/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 import datetime
-from ckeditor.fields import RichTextField
+from django_ckeditor_5.fields import CKEditor5Field
 
 class Post(models.Model):
     title = models.CharField(max_length=100, null=False)
     image = models.ImageField(null=True,blank=True,upload_to='media')
-    publish = RichTextField(blank=True)
+    publish = CKEditor5Field(blank=True)
     date = models.DateField(datetime.date.today)
 
